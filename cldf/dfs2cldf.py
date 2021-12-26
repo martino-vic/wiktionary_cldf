2d3f89ea00a5a6f390e8847ffc1aa2302750a0b1
Viktor Martinovic <viktor.martinovic@hotmail.com>
2021-12-26 19:44:49 +0000

    add dfs2cldf.py

---
+++ cldf/dfs2cldf.py
@@ -0,0 +1,142 @@
+"""convert csv-files to cldf and validate"""
+
+import pandas as pd
+import os
+import json
+import subprocess
+import logging
+import ast
+
+logging.basicConfig(filename='cldf.log',
+                    encoding='utf-8', level=logging.WARNING)
+
+
+class Csv2cldf:
+    """in: path to file, out: written cldf files"""
+
+    def __init__(self, folder: "'raw1' or 'raw2'", lang: str) -> None:
+        """initiate variables that are used by the class methods"""
+
+        glotto = "https://raw.githubusercontent.com/glottolog/glottolog-cldf"
+        self.dfglotto = pd.read_csv(f"{glotto}/master/cldf/languages.csv")
+        self.dfe = self.dfglotto[self.dfglotto["Name"] == "English"]\
+            .assign(Language_ID=0)
+        self.lg = lang.lower()
+        self.rp = "https://raw.githubusercontent.com/martino-vic/en_borrowings"
+        self.path = f"{self.rp}/master/{folder}/{self.lg}.csv"
+        self.meta = os.path.join(os.getcwd(), self.lg, "metadata.json")
+
+    def main(self) -> None:
+        """
+        create and write forms.csv, borrowings.csv, \
+        languages.csv, metadata.json, readme.md and validate.
+        """
+
+        self.metadata(self.forms(), self.borrowings())
+        self.lgs()
+        self.readme()
+
+    def readme(self) -> None:
+        """
+        Validate metadata.json. If validation passes write readme.md, \
+        else write error message to logfile cldf.log
+        """
+
+        rdm = os.path.join(os.getcwd(), self.lg, "readme.md")
+        try:  # check pycldf's documentation. write readme if validation passes
+            subprocess.run(f"cldf validate {self.meta}").check_returncode()
+            with open(rdm, 'w') as f:  # convert to readme
+                badge = f"[![CLDF validation]({self.rp}/master/cldf/badge.svg)](https://github.com/lexibank/uralex/actions?query=workflow%3ACLDF-validation)\n\n"
+                f.write(badge + subprocess.run(f"cldf markdown {self.meta}",
+                        capture_output=True).stdout.decode("utf-8")
+                        .replace("\r\n", "\n"))
+        except subprocess.CalledProcessError:  # else write error to logfile
+            logging.warning(subprocess.run(f"cldf validate {self.meta}",
+                            capture_output=True).stdout.decode("utf-8"))\
+                            .replace("\r\n", "\n")
+
+    def lgs(self) -> None:
+        """generate and write languages.csv"""
+
+        dfelocal = self.dfe
+        dflg = self.dfglotto[self.dfglotto["Name"] == self.lg]
+        if dflg.empty:
+            dfelocal = dfelocal.append(pd.DataFrame(
+                [["", self.lg.capitalize()] + [""]*(len(dfe.columns)-3) + [1]],
+                columns=dfe.columns))
+        else:
+            dfelocal = dfelocal.append(dflg.assign(Language_ID=1))
+
+        languages = os.path.join(os.getcwd(), self.lg, "languages.csv")
+        dfelocal.to_csv(languages, encoding="utf-8", index=False)
+
+    def metadata(self, lenform: int, lenborr: int) -> None:
+        """write metadata.json by inserting missing data into template"""
+
+        with open("metadata_template.json") as json_data:
+            data = json.load(json_data)
+
+        data['dc:title'] = data['dc:title'] + self.lg.capitalize()
+        data['rdf:ID'] = data['rdf:ID'] + self.lg
+        data['tables'][0]['dc:extent'] = lenform
+        data['tables'][2]['dc:extent'] = lenborr
+
+        with open(self.meta, "w") as j:
+            json.dump(data, j)
+
+    def forms(self) -> int:
+        """Generate and write forms.csv"""
+
+        dfm = pd.read_csv(self.path).rename(
+            columns={"L2_orth": "Form", "L2_ipa": "IPA", "L2_gloss": "Gloss"})
+        dfm["_1"] = ["" if isinstance(i, str) else i for i in dfm["L2_etym"]]
+        dfm["_2"] = dfm["_1"]  # two dummy cols for the loop
+
+        dfforms = pd.DataFrame()  # this will be the output
+        for form, donor in zip(["Form", "IPA", "Gloss"],
+                               ["L2_etym", "_1", "_2"]):
+            dfforms[form] = list(dfm[form]) + [i for i in dfm[donor]
+                                               if isinstance(i, str)]
+        dfforms["Language_ID"] = ["0"]*len(list(dfm[donor]))\
+            + ["1" for i in dfm[donor] if isinstance(i, str)]
+        dfforms.insert(0, "ID", dfforms.index)
+
+        try:
+            os.mkdir(self.lg)
+        except FileExistsError:
+            pass
+        fms = os.path.join(os.getcwd(), self.lg, "forms.csv")
+        dfforms.to_csv(fms, encoding="utf-8", index=False)
+
+        return len(dfforms)
+
+    def borrowings(self) -> int:
+        """Generate and write borrowings.csv"""
+
+        dfm = pd.read_csv(self.path)
+        dfm["ID"] = dfm.index
+
+        dfborr = pd.DataFrame()
+        dfborr["Target_Form_ID"] = [i for i, j in zip(dfm.ID, dfm.L2_etym)
+                                    if isinstance(j, str)]
+        dfborr["Source_Form_ID"] = [i for i in range(len(dfm),
+                                                     len(dfm)+len(dfborr))]
+        dfborr.insert(0, "ID", dfborr.index)
+
+        brr = os.path.join(os.getcwd(), self.lg, "borrowings.csv")
+        dfborr.to_csv(brr, encoding="utf-8", index=False)
+
+        return len(dfborr)
+
+
+def loop():
+    lglist = os.path.join(os.path.dirname(os.getcwd()), "lglist.txt")
+    ast.literal_eval(open(lglist, encoding="utf-8").read())
+    for lang, _ in zip(lglist, range(1000)):
+        try:
+            Csv2cldf(lang)
+        except:
+            pass
+
+if __name__ == "__main__":
+    Csv2cldf("raw1", "Greenlandic").main()
--- readme.md
+++ readme.md
@@ -2,19 +2,21 @@
 
 ## Content
 
-- dfs and dfs2 together contain 1403 csv-files of 125MB size in total. File names are languages as they appear on the English Wiktionary. Each file consist of 4 columns: 'L2_orth' representing the orthographical form of the word, 'L2_ipa' its IPA-transcription, 'L2_gloss' its English explanation and L2_etym the English word it originates from
+- raw1 and raw2 together contain 1403 csv-files of 125MB size in total. File names are languages as they appear on the English Wiktionary. Each file consist of 4 columns: 'L2_orth' representing the orthographical form of the word, 'L2_ipa' its IPA-transcription, 'L2_gloss' its English explanation and L2_etym the English word it originates from
 - lgs contains text-files with wordlists for every language that appears on the English Wiktionary. Files were created with WiktionaryParser.java.
 - WiktionaryParser.java is a courtesy of [Tomasz Jastrząb](https://scholar.google.com/citations?user=p2EcxaMAAAAJ&hl=pl) and was used to retrieve the wordlists found in the folder lgs 
 - lglist.txt is a [complete list of languages that appear on the English Wiktioanry](https://en.wiktionary.org/wiki/Wiktionary:List_of_languages).
 - lglist_full.txt is a copy of lglist.txt - since the latter serves as input for makedfs.py it can be modified according to one's needs without losing the full list.
 - LICENSE: MIT
-- makedfs.ipynb - The Parser with which the dfs were obtained from Wiktionary and assembled as data frames as an i-python-notebook.
-- makedfs.py - The same parser as makedfs.ipynb but as a .py file that can be run from the commandline by cd-ing into the right folder and running ```makedfs.py```
-With a download speed of 144Mbps the script needed 58 hours to parse all the languages from aari until zuni.
-- parser.log - There are a few bugs that are smoothened out during parsing that are documented here.
+- makedfs.py - The parser with which the csv files where obtained. With a download speed of 144Mbps the script needed 58 hours to parse all the languages from aari until zuni.
+- makedfs.ipynb - Some notes, documenting the parser's making-of
+- parser.log - Documenting corrupted file names and handling of errors that occured while squeezing parsed data into data frames
+- dfs is an empty folder into which the parser writes its results
 
 # remarks
-- Sometimes the column "L2_etym" is not displayed by the csv-viewer in Github. This is likely the case whenever the first 100 lines of the column are empty. Clicking on "raw" the column can be seen again.
+- Sometimes the column "L2_etym" is not displayed by the csv-viewer in Github. This is likely the case whenever the first 100 lines of the column are empty. Clicking on "raw", the column can be seen again.
+- The reason why columns have the "L2_" prefix is that this data was first used for baselinetests, where they served as pseudo-donor words (hence "L2" ~ second language ~ donor language), even though
+in the current setting they represent the recipient language (L1). The distinction L1-L2 is only internal.
 
 # Todo
 - convert raw data to cldf
